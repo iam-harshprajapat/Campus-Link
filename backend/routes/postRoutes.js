db73d66fcdd71e651e1b3a33b1daf13348e8e029
Harsh Prajapat <s.harshprajapat@gmail.com>
2024-10-14 22:37:35 +0530

    Added Comment feature to post

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPost, likePost } = require('../controllers/postController');
+const { createPost, likePost, addComment, deleteComment, getComments } = require('../controllers/postController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -10,5 +10,8 @@ router.post('/', authMiddleware, createPost);
 // Liking the post
 router.patch('/:id/like', authMiddleware, likePost)
 
-
+//comments
+router.post('/:postId/comments', authMiddleware, addComment);
+router.delete('/:postId/comments/:commentId', authMiddleware, deleteComment)
+router.get('/:postId/comments', authMiddleware, getComments);
 module.exports = router;
