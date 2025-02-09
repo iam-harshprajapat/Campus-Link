7785205a2ec0bfed174766b5d10c049b831d3801
iam-harshprajapat <s.harshprajapat@gmail.com>
2025-02-09 19:19:14 +0530

    minor change in cors

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://cl-campus-link.vercel.app",
-    methods: "GET, POST, PUT, DELETE",
+    methods: "GET, POST, PUT, DELETE, PATCH",
     credentials: true,
   })
 );
